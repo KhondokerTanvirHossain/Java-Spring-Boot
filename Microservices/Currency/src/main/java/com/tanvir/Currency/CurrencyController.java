e159770ee416029990237476e47a940d9805c19e
Tanvir <k.tanvir.hossain@gmail.com>
2020-03-25 15:47:24 +0600

    Microservice Complete

--- Microservices/Currency/src/main/java/com/tanvir/Currency/CurrencyController.java
+++ Microservices/Currency/src/main/java/com/tanvir/Currency/CurrencyController.java
@@ -1,5 +1,7 @@
 package com.tanvir.Currency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,17 +15,23 @@ import java.util.Map;
 
 @RestController
 public class CurrencyController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private CurrencyProxy proxy;
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public Currency convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
-//        Map<String , String> uriMap = new HashMap<>();
-//        uriMap.put("from", from);
-//        uriMap.put("to", to);
-//        ResponseEntity<Currency>  responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", Currency.class,
-//                uriMap);
-//        Currency response = responseEntity.getBody();
-          Currency response = proxy.retrieveExchangeValue(from, to);
+        Map<String , String> uriMap = new HashMap<>();
+        uriMap.put("from", from);
+        uriMap.put("to", to);
+        ResponseEntity<Currency>  responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", Currency.class,
+                uriMap);
+        Currency response = responseEntity.getBody();
+        return  new Currency(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public Currency convertCurrencyFeing(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+        Currency response = proxy.retrieveExchangeValue(from, to);
+        logger.info("{}", response);
         return  new Currency(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 
--- Microservices/Currency/src/main/java/com/tanvir/Currency/CurrencyProxy.java
+++ Microservices/Currency/src/main/java/com/tanvir/Currency/CurrencyProxy.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public Currency retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
